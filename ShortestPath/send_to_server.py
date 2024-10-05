46b317983c05c1e461b517d3b027456f0c88a584
kyumin1227 <kyumin12271227@gmail.com>
2024-10-05 21:13:32 +0900

    Feat: 아두이노 전달 값 계산 코드 추가
    - 아두이노 디스플레이에 전달 할 값 계산하는 코드 추가
    
    (차량 아이디, 구역 아이디, 표시할 화살표)

--- ShortestPath/send_to_server.py
+++ ShortestPath/send_to_server.py
@@ -2,6 +2,8 @@ import socketio
 import time
 import queue
 
+from shortest_route import walking_space
+
 # Standard Python
 sio = socketio.Client()
 
@@ -19,10 +21,37 @@ def send_to_server(uri, route_data_queue):
         try:
             # Queue에서 데이터가 있을 때까지 대기
             route_data = route_data_queue.get(timeout=1)
+            # Sending path: {'total_cars': 24, 'cars': {1: {'car_number': '1234', 'status': 'parking', 'parking': 22, 'route': [], 'parking_time': 1728125835.2989068},
+            #                                   2: {'car_number': '5678', 'status': 'parking', 'parking': 23, 'route': [], 'parking_time': 1728125835.298909}}}
             print(f"Sending path: {route_data}")
 
             # 서버로 데이터 전송
             sio.emit('message', route_data)
+
+            arduino_data = []
+
+            for car, value in route_data["cars"].items():
+                # 3개 이상의 경로가 있고 경로의 두 번째 값이 2, 4, 7, 9, 12, 14인 경우
+                route = value["route"]
+                if route and route[1] in (2, 4, 7, 9, 12, 14) and len(route) > 2:
+                    display_area = walking_space[route[1]]
+                    next_area = walking_space[route[2]]
+                    display_area_id = route[1]
+
+                    # TODO 실환경에서 테스트 할 경우에는 좌표가 완전히 동일하지 않기 때문에 +- 얼마 정도로 조건을 수정해야 함
+                    # x 좌표가 같은 경우
+                    if display_area["position"][0][0] == next_area["position"][0][0]:
+                        if display_area["position"][0][1] < next_area["position"][0][1]:
+                            arduino_data.append((car, display_area_id, "down"))
+                        elif display_area["position"][0][1] > next_area["position"][0][1]:
+                            arduino_data.append((car, display_area_id, "up"))
+
+                    # y 좌표가 같은 경우
+                    elif display_area["position"][0][1] == next_area["position"][0][1]:
+                        if display_area["position"][0][0] < next_area["position"][0][0]:
+                            arduino_data.append((car, display_area_id, "right"))
+                        elif display_area["position"][0][0] > next_area["position"][0][0]:
+                            arduino_data.append((car, display_area_id, "left"))
 
         except queue.Empty:
             # Queue가 비었을 때는 잠시 대기
