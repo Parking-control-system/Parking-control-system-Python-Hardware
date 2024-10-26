ed29faa3fc12b231962e24831fabaf9c85f718c4
kyumin1227 <kyumin12271227@gmail.com>
2024-10-26 23:48:51 +0900

    Feat: 웹 페이지의 비율에 맞게 좌표 수정
    - 이동하는 차량의 좌표를 웹 페이지의 비율에 맞게 수정하여 전송

--- ShortestPath/send_to_server.py
+++ ShortestPath/send_to_server.py
@@ -3,8 +3,8 @@ import time
 import queue
 import json
 import serial
-
-from test import serial_port
+import numpy as np
+import cv2
 
 
 # 사각형의 중심 계산 함수
@@ -14,6 +14,12 @@ def calculate_center(points):
     center_x = sum(x_coords) / len(points)
     center_y = sum(y_coords) / len(points)
     return (center_x, center_y)
+
+def transform_to_web_coordinates(zone_id, x, y):
+    """주차장 좌표를 웹 좌표로 변환"""
+    point = np.array([[[x, y]]], dtype="float32")
+    transformed_point = cv2.perspectiveTransform(point, transform_matrices[zone_id])
+    return transformed_point[0][0]
 
 # 소켓 지정
 sio = socketio.Client(reconnection=True, reconnection_attempts=5, reconnection_delay=2)
@@ -38,6 +44,41 @@ def send_to_server(uri, route_data_queue, parking_space_path, walking_space_path
         walking_space = json.load(f)
         walking_space = {int(key): value for key, value in walking_space.items()}  # 문자열 키를 숫자로 변환
 
+    web_coordinates = {
+        1: [(10, 160), (10, 240)],
+        2: [(10, 160), (350, 240)],
+        3: [(350, 160), (710, 240)],
+        4: [(710, 160), (1200, 240)],
+        5: [(10, 240), (350, 495)],
+        6: [(710, 240), (1200, 495)],
+        7: [(10, 495), (350, 600)],
+        8: [(350, 495), (710, 600)],
+        9: [(710, 495), (1200, 600)],
+        10: [(10, 600), (350, 845)],
+        11: [(710, 600), (1200, 845)],
+        12: [(10, 845), (350, 920)],
+        13: [(350, 845), (710, 920)],
+        14: [(710, 845), (1200, 920)],
+        15: [(10, 845), (10, 920)]
+    }
+
+    for zone_id, parking_pts in walking_space.items():
+        # 주차장의 네 개 좌표를 numpy 배열로 변환
+        parking_pts_array = np.array(parking_pts["position"], dtype="float32")
+
+        # 웹 구역의 네 개 좌표 생성 (직사각형이므로 자동으로 구할 수 있음)
+        web_top_left, web_bottom_right = web_coordinates[zone_id]
+        web_pts_array = np.array([
+            [web_top_left[0], web_top_left[1]],
+            [web_bottom_right[0], web_top_left[1]],
+            [web_bottom_right[0], web_bottom_right[1]],
+            [web_top_left[0], web_bottom_right[1]]
+        ], dtype="float32")
+
+        # 투시 변환 행렬 계산 (주차장에서 웹으로 변환)
+        transform_matrix = cv2.getPerspectiveTransform(parking_pts_array, web_pts_array)
+        transform_matrices[zone_id] = transform_matrix
+
     while True:
         try:
             # Queue에서 데이터가 있을 때까지 대기
@@ -58,15 +99,15 @@ def send_to_server(uri, route_data_queue, parking_space_path, walking_space_path
 
             print("cars (send to server) = ", cars)
 
-            # TODO 차량의 위치 데이터를 비율 계산하여서 전송
             for id, value in cars.items():
                 print("id = ", id)
                 print("value = ", value)
+                transformed_x, transformed_y = transform_to_web_coordinates(id, value["position"][0], value["position"][1])
                 if value["status"] == "entry":
                     parking_data[value["parking"]]["entry_time"] = value["entry_time"]
-                    moving_data[id] = {"position": value["position"], "entry_time": value["entry_time"], "car_number": value["car_number"], "status": "entry"}
+                    moving_data[id] = {"position": (transformed_x, transformed_y), "entry_time": value["entry_time"], "car_number": value["car_number"], "status": "entry"}
                 elif value["status"] == "exit":
-                    moving_data[id] = {"position": value["position"], "entry_time": value["entry_time"], "car_number": value["car_number"], "status": "exit"}
+                    moving_data[id] = {"position": (transformed_x, transformed_y), "entry_time": value["entry_time"], "car_number": value["car_number"], "status": "exit"}
 
             send_data["parking"] = parking_data
             send_data["moving"] = moving_data
@@ -75,7 +116,6 @@ def send_to_server(uri, route_data_queue, parking_space_path, walking_space_path
 
             # 서버로 데이터 전송
             sio.emit('message', send_data)
-
 
             # Arduino로 전송할 데이터 생성
             arduino_data = {}
@@ -119,6 +159,8 @@ def send_to_server(uri, route_data_queue, parking_space_path, walking_space_path
             # Queue가 비었을 때는 잠시 대기
             time.sleep(1)
             continue
+
+transform_matrices = {}
 
 if __name__ == "__main__":
 
