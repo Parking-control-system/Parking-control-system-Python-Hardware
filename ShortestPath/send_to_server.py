198f97865ab0fdd5073d0bbfbdad5b97aad16a49
kyumin1227 <kyumin12271227@gmail.com>
2024-11-19 20:51:25 +0900

    Refactor: 서버 전달 쓰레드 함수화

--- ShortestPath/send_to_server.py
+++ ShortestPath/send_to_server.py
@@ -10,6 +10,37 @@ import cv2
 import platform
 
 
+# 웹 페이지 각 구역 좌표
+web_coordinates = {
+        1: [(-50, 100), (0, 200)],
+        2: [(0, 100), (300, 200)],
+        3: [(300, 100), (650, 200)],
+        4: [(650, 100), (1150, 200)],
+        5: [(10, 200), (300, 430)],
+        6: [(650, 200), (1150, 430)],
+        7: [(0, 430), (300, 550)],
+        8: [(350, 430), (650, 550)],
+        9: [(650, 430), (1150, 550)],
+        10: [(0, 550), (300, 780)],
+        11: [(650, 550), (1150, 780)],
+        12: [(0, 780), (300, 860)],
+        13: [(300, 780), (650, 860)],
+        14: [(650, 780), (1150, 860)],
+        15: [(-50, 780), (0, 860)]
+}
+
+# 아두이노로 전송할 데이터
+arduino_data = {}
+
+# 이전에 아두이노로 전송한 데이터
+previous_arduino_data = None
+
+# 경로를 안내하는 디스플레이의 구역 번호 (아두이노의 메트릭스 순서에 맞게 조정)
+DISPLAY_SPACE = (12, 7, 2, 14, 9, 4)
+
+# 이동 구역의 좌표
+walking_space = {}
+
 # 사각형의 중심점 계산 함수
 def calculate_center(points):
     x_coords = [p[0] for p in points]
@@ -52,6 +83,7 @@ def transform_point_in_quadrilateral_to_rectangle(point, quadrilateral, arg_web_
     transformed_x, transformed_y = transformed_point[0][0]
     return float(transformed_x), float(transformed_y)
 
+
 def reflect_point_in_rectangle(point, rectangle_corners):
     """
     직사각형의 좌상단과 우하단 좌표만을 이용해 특정 점을 상하좌우 반전시킨 좌표로 변환합니다.
@@ -75,6 +107,44 @@ def reflect_point_in_rectangle(point, rectangle_corners):
     return reflected_x, reflected_y
 
 
+# 경로에 따라 아두이노로 전송할 데이터 생성
+def set_arduino_data(route, value):
+    display_area = walking_space[route[1]]
+    next_area = walking_space[route[2]]
+    display_area_id = DISPLAY_SPACE.index(route[1]) + 1
+
+    display_center = calculate_center(display_area["position"])  # display_area의 중심점
+    next_center = calculate_center(next_area["position"])  # next_area의 중심점
+
+    # display_area와 next_area의 중심점 좌표 차이 계산
+    delta_x = abs(display_center[0] - next_center[0])
+    delta_y = abs(display_center[1] - next_center[1])
+
+    # X 좌표의 차이가 더 큰 경우
+    if delta_x > delta_y:
+        if display_center[0] < next_center[0]:
+            arduino_data[display_area_id] = {"car_number": value.get("car_number", "No Number"), "direction": "left"}
+        elif display_center[0] > next_center[0]:
+            arduino_data[display_area_id] = {"car_number": value.get("car_number", "No Number"), "direction": "right"}
+
+    # Y 좌표의 차이가 더 큰 경우
+    else:
+        if display_center[1] < next_center[1]:
+            arduino_data[display_area_id] = {"car_number": value.get("car_number", "No Number"), "direction": "up"}
+        elif display_center[1] > next_center[1]:
+            arduino_data[display_area_id] = {"car_number": value.get("car_number", "No Number"), "direction": "down"}
+
+
+def cal_web_position(space_id, car_id, cars):
+    transformed_x, transformed_y = transform_point_in_quadrilateral_to_rectangle(cars[car_id]["position"],
+                                                                                 walking_space[space_id]["position"],
+                                                                                 web_coordinates[space_id])
+
+    reflect_x, reflect_y = reflect_point_in_rectangle((transformed_x, transformed_y), web_coordinates[space_id])
+
+    return reflect_x, reflect_y
+
+
 # 소켓 지정
 sio = socketio.Client(reconnection=True, reconnection_attempts=5, reconnection_delay=2)
 
@@ -88,10 +158,14 @@ def disconnect():
 
 def send_to_server(uri, route_data_queue, parking_space_path, walking_space_path, serial_port, serial_port2):
     # 서버 연결
-    global previous_serial_data
-    sio.connect(uri)
-    # 시리얼 통신 설정
+    global arduino_data
+    global previous_arduino_data
+    global walking_space
 
+    # 서버 연결
+    sio.connect(uri)
+
+    # 시리얼 통신 설정
     if platform.system() == "Linux":
         ser = serial.Serial(serial_port, 9600, timeout=1)
         ser2 = serial.Serial(serial_port2, 9600, timeout=1)
@@ -110,40 +184,38 @@ def send_to_server(uri, route_data_queue, parking_space_path, walking_space_path
             #                                   2: {'car_number': '5678', 'status': 'parking', 'parking': 23, 'route': [], 'parking_time': 1728125835.298909}}}
 
             print(f"send_to_server 에서 받은 데이터 : {data}")
-            cars = data["cars"]
+            cars = data["cars"] # 차량 데이터    {car_id: {car_number, status, parking, route, parking_time}}
+            parking_data = data["parking"]  # 주차 구역 데이터 {space_id: {name, status, car_id, car_number, position, entry_time, exit_time}}
 
-            parking_data = data["parking"]
+            send_data = {"time": time.time()}   # 서버로 전송할 데이터
 
-            send_data = {"time": time.time()}
+            moving_data = {}    # 이동 중인 차량 데이터
 
-            moving_data = {}
-
-            for id, value in cars.items():
-                print("id = ", id)
+            for car_id, value in cars.items():
+                print("car_id = ", car_id)
                 print("value = ", value)
                 if value["status"] == "entry":
                     parking_data[value["parking"]]["entry_time"] = value["entry_time"]
-                    moving_data[id] = {"entry_time": value["entry_time"], "car_number": value["car_number"], "status": "entry"}
+                    moving_data[car_id] = {"entry_time": value["entry_time"], "car_number": value["car_number"], "status": value["status"]}
                 elif value["status"] == "exit":
-                    moving_data[id] = {"entry_time": value["entry_time"], "car_number": value["car_number"], "status": "exit"}
+                    moving_data[car_id] = {"entry_time": value["entry_time"], "car_number": value["car_number"],"status": value["status"]}
 
             print("movingData = ", moving_data)
 
             # 이동 중인 차량 좌표 계산
-            walking_cars = data["walking"]
+            walking_cars = data["walking"]  # 이동 중인 차량 데이터 {space_id: car_id}
             print("walking_cars", walking_cars)
 
-            for id, value in walking_cars.items():
+            for space_id, car_id in walking_cars.values():
                 # 차량 목록에 없는 경우 무시
-                if value not in cars:
+                if car_id not in cars:
                     continue
-                transformed_x, transformed_y = transform_point_in_quadrilateral_to_rectangle(cars[value]["position"],
-                                                                                             walking_space[id]["position"],
-                                                                                             web_coordinates[id])
 
-                reflect_x, reflect_y = reflect_point_in_rectangle((transformed_x, transformed_y), web_coordinates[id])
-                moving_data[value]["position"] = (reflect_x, reflect_y)
+                # 웹페이지에 표시할 좌표 계산
+                x, y = cal_web_position(space_id, car_id, cars)
+                moving_data[car_id]["position"] = (x, y)
 
+            # 전송할 데이터 세팅
             send_data["parking"] = parking_data
             send_data["moving"] = moving_data
 
@@ -152,43 +224,21 @@ def send_to_server(uri, route_data_queue, parking_space_path, walking_space_path
             # 서버로 데이터 전송
             sio.emit('message', send_data)
 
-            # Arduino로 전송할 데이터 생성
-            arduino_data = {}
-
+            # Arduino로 전송할 데이터 초기화
+            arduino_data.clear()
+            # 차량의 경로에 따라 아두이노로 전송할 데이터 생성
             for car_id, value in data["cars"].items():
                 # 3개 이상의 경로가 있고 경로의 두 번째 값이 2, 4, 7, 9, 12, 14인 경우
                 route = value["route"]
                 if route and len(route) > 2 and route[1] in DISPLAY_SPACE:
-                    display_area = walking_space[route[1]]
-                    next_area = walking_space[route[2]]
-                    display_area_id =  DISPLAY_SPACE.index(route[1]) + 1
-
-                    display_center = calculate_center(display_area["position"])  # display_area의 중심점
-                    next_center = calculate_center(next_area["position"])  # next_area의 중심점
-
-                    # display_area와 next_area의 중심점 좌표 차이 계산
-                    delta_x = abs(display_center[0] - next_center[0])
-                    delta_y = abs(display_center[1] - next_center[1])
-
-                    # X 좌표의 차이가 더 큰 경우
-                    if delta_x > delta_y:
-                        if display_center[0] < next_center[0]:
-                            arduino_data[display_area_id] = {"car_number": value.get("car_number", "No Number"), "direction": "left"}
-                        elif display_center[0] > next_center[0]:
-                            arduino_data[display_area_id] = {"car_number": value.get("car_number", "No Number"), "direction": "right"}
-
-                    # Y 좌표의 차이가 더 큰 경우
-                    else:
-                        if display_center[1] < next_center[1]:
-                            arduino_data[display_area_id] = {"car_number": value.get("car_number", "No Number"), "direction": "up"}
-                        elif display_center[1] > next_center[1]:
-                            arduino_data[display_area_id] = {"car_number": value.get("car_number", "No Number"), "direction": "down"}
+                    set_arduino_data(route, value)
 
             print(f"Arduino data: {arduino_data}")
-            print(f"Previous serial data: {previous_serial_data}")
+            print(f"Previous arduino data: {previous_arduino_data}")
 
-            if arduino_data != previous_serial_data:
-                previous_serial_data = arduino_data
+            # 생성한 데이터가 이전 데이터와 다를 경우 아두이노로 데이터 전송
+            if arduino_data != previous_arduino_data:
+                previous_arduino_data = arduino_data
                 if platform.system() == "Linux":
                     ser.write((str(arduino_data) + "\n").encode())
                     ser2.write((str(arduino_data) + "\n").encode())
@@ -199,29 +249,6 @@ def send_to_server(uri, route_data_queue, parking_space_path, walking_space_path
             print("Queue is empty")
             time.sleep(1)
             continue
-
-web_coordinates = {
-        1: [(-50, 100), (0, 200)],
-        2: [(0, 100), (300, 200)],
-        3: [(300, 100), (650, 200)],
-        4: [(650, 100), (1150, 200)],
-        5: [(10, 200), (300, 430)],
-        6: [(650, 200), (1150, 430)],
-        7: [(0, 430), (300, 550)],
-        8: [(350, 430), (650, 550)],
-        9: [(650, 430), (1150, 550)],
-        10: [(0, 550), (300, 780)],
-        11: [(650, 550), (1150, 780)],
-        12: [(0, 780), (300, 860)],
-        13: [(300, 780), (650, 860)],
-        14: [(650, 780), (1150, 860)],
-        15: [(-50, 780), (0, 860)]
-}
-
-previous_serial_data = None
-
-# 경로를 안내하는 디스플레이의 구역 번호
-DISPLAY_SPACE = (12, 7, 2, 14, 9, 4)
 
 
 if __name__ == "__main__":
